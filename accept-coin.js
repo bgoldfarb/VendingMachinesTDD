20e982057e2266acf8f616410bcb7ae5a33b9596
bgoldfa <bgoldfa@gap.com>
2017-12-03 17:21:01 -0500

    Exporting coins to seperate file:

--- accept-coin.js
+++ accept-coin.js
@@ -1,35 +1,13 @@
-var quarter = {
-    value: 0.25,
-    weight: 5.670,
-    size: 24.26
-}
-
-var nickel = {
-    value: 0.05,
-    weight: 5,
-    size: 21.21
-}
-
-var dime = {
-    value: 0.10,
-    weight: 2.268,
-    size: 17.91
-}
-
-var penny = {
-    value: 0.01,
-    weight: 2.5,
-    size: 19.05
-}
+var coins = require('./coins')
 
 
 var acceptCoin = {
 
-    isNickel: (nickelSize, nickelWeight) => (nickelSize === nickel.size && nickelWeight === nickel.weight ? true : false),
-    isDime: (dimeSize, dimeWeight) => (dimeSize === dime.size && dimeWeight === dime.weight ? true : false),
-    isQuarter: (quarterSize, quarterWeight) => (quarterSize === quarter.size && quarterWeight === quarterWeight ? true : false),
+    isNickel: (nickelSize, nickelWeight) => (nickelSize === coins.nickel.size && nickelWeight === coins.nickel.weight? true : false),
+    isDime: (dimeSize, dimeWeight) => (dimeSize === coins.dime.size && dimeWeight === coins.dime.weight ? true : false),
+    isQuarter: (quarterSize, quarterWeight) => (quarterSize === coins.quarter.size && quarterWeight === coins.quarter.weight ? true : false),
     isAcceptableCoin: (coinSize, coinWeight) => (acceptCoin.isNickel(coinSize, coinWeight) || acceptCoin.isDime(coinSize, coinWeight) || acceptCoin.isQuarter(coinSize, coinWeight))
 };
 
 
-module.exports = acceptCoin;
+module.exports = acceptCoin;      
---
+++ coins.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    quarter: {
+        value: 0.25,
+        weight: 5.670,
+        size: 24.26
+    },
+    
+     nickel: {
+        value: 0.05,
+        weight: 5,
+        size: 21.21
+    },
+    
+     dime: {
+        value: 0.10,
+        weight: 2.268,
+        size: 17.91
+    },
+    
+     penny: {
+        value: 0.01,
+        weight: 2.5,
+        size: 19.05
+    }
+
+
+} 
