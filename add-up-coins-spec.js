9d8915e6b22f90f75cc483dd35085fb35aee100b
bgoldfa <bgoldfa@gap.com>
2017-12-04 14:14:59 -0500

    Refactoring...

--- add-up-coins-spec.js
+++ add-up-coins-spec.js
@@ -1,59 +1,54 @@
 'use strict';
 var expect = require('chai').expect;
 
-var addUpCoins = ""
+var addUpCoins = require('./add-up-coins');
 var coins = require('./coins')
 
 
 var coinsToBeInserted = [coins.quarter, coins.nickel, coins.dime, coins.dime, coins.quarter]
-var coinsToBeInsertedWithBadCoins= [coins.quarter, coins.penny, coins.dime, coins.dime, coins.quarter, coins.nickel]
+var coinsToBeInsertedWithBadCoins = [coins.quarter, coins.penny, coins.dime, coins.dime, coins.quarter, coins.nickel]
 var noCoinsToBeInserted = []
 
 
 describe('All tests', () => {
 
-beforeEach(function(){
-    addUpCoins = require('./add-up-coins');
 
-})
-
-
-describe('add-up-coin-exists', function() {
-    it('should exist', function() {
-        expect(addUpCoins).to.not.be.undefined;
+    describe('add-up-coin-exists', function () {
+        it('should exist', function () {
+            expect(addUpCoins).to.not.be.undefined;
+        });
     });
-});
 
 
 
-describe('Coin Sum Functionality', function() {
+    describe('Coin Sum Functionality', function () {
 
 
-    it('should extract all values from coin objects', function(){
-        var quarterValue = addUpCoins.extractCoinValue(coins.quarter)
-        var dimeValue = addUpCoins.extractCoinValue(coins.dime)
-        var nickelValue = addUpCoins.extractCoinValue(coins.nickel)
-        expect(quarterValue).to.be.equal(0.25)
-        expect(dimeValue).to.be.equal(0.10)
-        expect(nickelValue).to.be.equal(0.05)
-    })
+        it('should extract all values from coin objects', function () {
+            var quarterValue = addUpCoins.extractCoinValue(coins.quarter)
+            var dimeValue = addUpCoins.extractCoinValue(coins.dime)
+            var nickelValue = addUpCoins.extractCoinValue(coins.nickel)
+            expect(quarterValue).to.be.equal(0.25)
+            expect(dimeValue).to.be.equal(0.10)
+            expect(nickelValue).to.be.equal(0.05)
+        })
 
-    it('should add up all coins', function(){
-        var sum = addUpCoins.determineCoinSum(coinsToBeInserted)
-        expect(sum).to.be.equal(0.75)
-    })
+        it('should add up all coins', function () {
+            var sum = addUpCoins.determineCoinSum(coinsToBeInserted)
+            expect(sum).to.be.equal(0.75)
+        })
 
-    it('should add up all coins and ignore the bad coins', function(){
-        var sum = addUpCoins.determineCoinSum(coinsToBeInsertedWithBadCoins)
-        expect(sum).to.be.equal(0.75)
-    })
+        it('should add up all coins and ignore the bad coins', function () {
+            var sum = addUpCoins.determineCoinSum(coinsToBeInsertedWithBadCoins)
+            expect(sum).to.be.equal(0.75)
+        })
 
-    it('should return all the bad coins', function(){
-       var coins = addUpCoins.returnRejectedCoins()
-       expect(coins).to.be.equal(0.01)
-    })
+        it('should return all the bad coins', function () {
+            var coins = addUpCoins.returnRejectedCoins()
+            expect(coins).to.be.equal(0.01)
+        })
 
 
-});
+    });
 
 })
--- add-up-coins.js
+++ add-up-coins.js
@@ -1,36 +1,30 @@
 import {
     map,
     reduce,
-    filter
+    filter,
+    chain
 } from 'lodash'
 
 var acceptCoin = require('./accept-coin');
-
 var rejectedCoins = {
     value: 0
 }
 
 var determineCoin = require('./accept-coin');
-
+var sum = 0;
 var addUpCoins = {
 
-    extractCoinValue:  (coin) =>  coin.value ,
+    extractCoinValue: (coin) => coin.value,
 
-    determineCoinSum:  (coins) => {
+    /*Then you can split all your coins into two arrays, accepted and rejected. Then you can sum of the value of the rejected array for that value
+    */
+    determineCoinSum: (coins) => {
         if (coins.length > 0) {
-            var acceptableCoinArray = filter(coins, function (i) {
-                if (acceptCoin.isAcceptableCoin(i.size, i.weight)) {
-                    return i
-                } else {
-                    rejectedCoins.value += i.value;
-                }
-            })
-            var sum = 0;
-            var valueArray = map(acceptableCoinArray, function (item) {
-                return item.value
-            })
-            sum = reduce(valueArray, (sum, n) => sum + n, 0)
-
+            sum = chain(coins)
+                .filter((i) => (acceptCoin.isAcceptableCoin(i.size, i.weight) ? i : false ))
+                .map((item) => item.value)
+                .reduce((sum, n) => sum + n, 0)
+                .value()
             return sum;
         } else {
             return "INSERT COINS"
